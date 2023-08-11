03b16804877e8b8b32ce99828b239f0fe903618c
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-08-11 19:26:04 +0200

    edit

---
+++ src/test/java/com/myProject/pages/ContactUsPage.java
@@ -0,0 +1,50 @@
+package com.myProject.pages;
+
+import com.myProject.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactUsPage extends BasePage{
+    Actions actions = new Actions(Driver.get());
+
+    @FindBy(xpath = "//h2[.='Get In Touch']")
+    public WebElement getInTouchText;
+
+    @FindBy(xpath = "//input[@data-qa='name']")
+    public WebElement nameBox;
+
+    @FindBy(xpath = "//input[@data-qa='submit-button']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//input[@name='upload_file']")
+    public WebElement chooseFileButton;
+
+    @FindBy(xpath = "//div[contains(text(),'Success!')][1]")
+    public WebElement successText;
+
+    @FindBy(xpath = "//span[.=' Home']")
+    public WebElement homeButton;
+
+    @FindBy(id = "dismiss-button")
+    public WebElement dismissButton;
+
+    public void fillContactUsForm(){
+        actions.click(nameBox)
+                .sendKeys("myName" + Keys.TAB)
+                .sendKeys("myEmail@email.com" + Keys.TAB)
+                .sendKeys("mySubject" + Keys.TAB)
+                .sendKeys("myMessage" + Keys.TAB)
+                .perform();
+    }
+
+    public void uploadFile(){
+        String projectPath = System.getProperty("user.dir");
+        String filePath = "src/test/resources/test.txt";
+        String fullPath = projectPath + "/" + filePath;
+        chooseFileButton.sendKeys(fullPath);
+    }
+
+
+}
---
+++ src/test/resources/features/contactUs.feature
@@ -0,0 +1,18 @@
+Feature: Contact Us Form Feature
+
+  @contact
+  Scenario: Contact Us Form
+    Given The user is on the home page
+    When The user clicks "Contact us" menu button
+    Then Verify GET IN TOUCH is visible
+    When The user enters name, email, subject and message
+    And The user uploads file
+    And The User clicks Submit button
+    And The user clicks OK button
+    Then Verify success message Success! Your details have been submitted successfully. is visible
+    When The user clicks Home button
+    Then Verify that landed to home page successfully
+
+
+
+
