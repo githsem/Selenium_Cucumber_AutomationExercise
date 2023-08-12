3c59658a688af2efd08ce19f974a123f824a8f1f
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-08-12 23:25:59 +0200

    edit

---
+++ src/test/java/com/myProject/stepDefinitions/CategoryProduct_StepDefs.java
@@ -0,0 +1,35 @@
+package com.myProject.stepDefinitions;
+
+import com.myProject.pages.CategoryProductPage;
+import com.myProject.utilities.BrowserUtils;
+import com.myProject.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Set;
+
+public class CategoryProduct_StepDefs {
+    CategoryProductPage categoryProductPage = new CategoryProductPage();
+
+    @Then("Verify that category page is displayed and confirm text women Dress PRODUCTS")
+    public void verify_that_category_page_is_displayed_and_confirm_text_women_dress_products() {
+        BrowserUtils.waitForVisibility(categoryProductPage.womenDressProductsText,5);
+        Assert.assertEquals("WOMEN - DRESS PRODUCTS",categoryProductPage.womenDressProductsText.getText());
+    }
+
+    @When("The user clicks on Men category")
+    public void the_user_clicks_on_men_category() {
+        categoryProductPage.menCategory.click();
+    }
+
+    @When("The user clicks on Tshirts link under Men category")
+    public void the_user_clicks_on_tshirts_link_under_men_category() {
+        categoryProductPage.tshirtsCategory.click();
+    }
+
+    @Then("Verify that category page is displayed and confirm text men Tshirts PRODUCTS")
+    public void verify_that_category_page_is_displayed_and_confirm_text_men_tshirts_products() {
+        Assert.assertEquals("MEN - TSHIRTS PRODUCTS",categoryProductPage.menTshirtsProductsText.getText());
+    }
+}
