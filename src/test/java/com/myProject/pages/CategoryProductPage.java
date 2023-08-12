6318f51ab2f5869949ea3883d372ecaba83f3381
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-08-12 23:26:08 +0200

    edit

---
+++ src/test/java/com/myProject/pages/CategoryProductPage.java
@@ -0,0 +1,20 @@
+package com.myProject.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CategoryProductPage extends BasePage{
+
+    @FindBy(xpath = "//h2[.='Women - Dress Products']")
+    public WebElement womenDressProductsText;
+
+    @FindBy(xpath = "//a[@href='#Men']")
+    public WebElement menCategory;
+
+    @FindBy(xpath = "//a[.='Tshirts ']")
+    public WebElement tshirtsCategory;
+
+    @FindBy(xpath = "//h2[.='Men - Tshirts Products']")
+    public WebElement menTshirtsProductsText;
+
+}
