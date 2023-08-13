af60152338b1178a0f4009ec94d310fb7a0e2bda
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-08-13 21:23:47 +0200

    edit

---
+++ src/test/java/com/myProject/pages/SearchPage.java
@@ -0,0 +1,17 @@
+package com.myProject.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchPage extends BasePage{
+    @FindBy(xpath = "//h2[.='Searched Products']")
+    public WebElement searchedProductsText;
+
+    @FindBy(xpath = "//div[@class='features_items']/div[@class=\"col-sm-4\"]")
+    public List<WebElement> searchedProducts;
+
+
+
+}
---
+++ src/test/java/com/myProject/stepDefinitions/Search_StepDefs.java
@@ -0,0 +1,28 @@
+package com.myProject.stepDefinitions;
+
+import com.myProject.pages.ProductsPage;
+import com.myProject.pages.SearchPage;
+import com.myProject.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
+public class Search_StepDefs {
+    ProductsPage productsPage = new ProductsPage();
+    SearchPage searchPage = new SearchPage();
+
+    @When("The user enters product name in search input and click search button")
+    public void the_user_enters_product_name_in_search_input_and_click_search_button() {
+        productsPage.searchProductBox.sendKeys("Kids" + Keys.TAB + Keys.ENTER);
+    }
+    @Then("Verify SEARCHED PRODUCTS is visible")
+    public void verify_searched_products_is_visible() {
+        BrowserUtils.verifyElementDisplayed(searchPage.searchedProductsText);
+    }
+    @Then("Verify all the products related to search are visible")
+    public void verify_all_the_products_related_to_search_are_visible() {
+        Assert.assertTrue(searchPage.searchedProducts.size()==1);
+    }
+
+}
