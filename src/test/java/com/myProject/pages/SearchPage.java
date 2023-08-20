b4f89da5039e0e51c406ff4cbf5269b9288fde6c
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-08-20 18:26:41 +0200

    modified

--- src/test/java/com/myProject/pages/SearchPage.java
+++ src/test/java/com/myProject/pages/SearchPage.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
-public class SearchPage extends BasePage{
+public class SearchPage extends BasePage {
     @FindBy(xpath = "//h2[.='Searched Products']")
     public WebElement searchedProductsText;
 
     @FindBy(xpath = "//div[@class='features_items']/div[@class=\"col-sm-4\"]")
     public List<WebElement> searchedProducts;
-
 
 
 }
--- src/test/java/com/myProject/stepDefinitions/Search_StepDefs.java
+++ src/test/java/com/myProject/stepDefinitions/Search_StepDefs.java
@@ -16,13 +16,15 @@ public class Search_StepDefs {
     public void the_user_enters_product_name_in_search_input_and_click_search_button() {
         productsPage.searchProductBox.sendKeys("Kids" + Keys.TAB + Keys.ENTER);
     }
+
     @Then("Verify SEARCHED PRODUCTS is visible")
     public void verify_searched_products_is_visible() {
         BrowserUtils.verifyElementDisplayed(searchPage.searchedProductsText);
     }
+
     @Then("Verify all the products related to search are visible")
     public void verify_all_the_products_related_to_search_are_visible() {
-        Assert.assertTrue(searchPage.searchedProducts.size()==1);
+        Assert.assertTrue(searchPage.searchedProducts.size() == 1);
     }
 
 }
